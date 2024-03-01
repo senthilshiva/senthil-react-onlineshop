de3c67c0cf1e03abe21d78678c59d8092a0a61dc
senthilshiva <senthilmca123@gmail.com>
2024-03-01 18:15:02 +0530

    heelo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import { Footer } from './components/layouts/Footer';
+import Header from './components/layouts/Header';
+import Home from './components/layouts/Home';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header/>
+      <Home/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
